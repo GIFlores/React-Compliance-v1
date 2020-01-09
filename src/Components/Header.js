b436ff314e3d92a0a8583e36d6d2c3b45697b24d
Greg <gregian_flores@yahoo.com>
2020-01-09 11:43:01 +0800

    'commit'

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <p>header fuck</p>
+                <p>This is a Header</p>
             </div>
         )
             
